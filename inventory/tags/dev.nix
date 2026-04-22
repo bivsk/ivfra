aaddef187e4e6e9daec2e100c9c0f31c50c6e7a7
bivsk <bivsk@tutanota.com>
2026-04-22 06:50:45 -0400

    android: add users to adbusers group

--- inventory/tags/dev.nix
+++ inventory/tags/dev.nix
@@ -1,16 +1,46 @@
-{ pkgs, ... }:
 {
-  programs.direnv.enable = true;
-  environment.systemPackages = with pkgs; [
-    bind
-    comma
-    dig
-    ethtool
-    gh
-    lsof
-    nixpkgs-review
-    net-tools
-    nmap
-    socat
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  inherit (lib)
+    attrNames
+    const
+    filterAttrs
+    getAttr
+    ;
+
+  inherit (config.home-manager.users.four) xdg;
+in
+{
+  programs = {
+    direnv.enable = true;
+
+    nh = {
+      enable = true;
+      flake = "${xdg.userDirs.projects}/${config.clan.core.settings.name}";
+    };
+  };
+
+  environment.systemPackages = [
+    pkgs.bind
+    pkgs.comma
+    pkgs.dig
+    pkgs.ethtool
+    pkgs.gh
+    pkgs.lsof
+    pkgs.nixpkgs-review
+    pkgs.net-tools
+    pkgs.nmap
+    pkgs.socat
+
+    # android
+    pkgs.android-tools
+    pkgs.jmtpfs
   ];
+
+  users.extraGroups.adbusers.members =
+    config.users.users |> filterAttrs (const <| getAttr "isNormalUser") |> attrNames;
 }
